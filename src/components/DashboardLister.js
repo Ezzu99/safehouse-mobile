28e82c758a395016438818a33edec3895b808c49
Ezzu99 <ezaan1999.ali@gmail.com>
2022-12-07 08:56:11 +0500

    Updated Lister dashboard

--- src/components/DashboardLister.js
+++ src/components/DashboardLister.js
@@ -32,13 +32,13 @@ const DashboardLister = () => {
     const [jobs, setJobs] = useState();
     const [showWorkshop, setShowWorkshop] = useState(true);
     const [showJob, setShowJob] = useState(false);
-    const [showAddJob, setShowAddJob] = useState(false);
+    const [showAddJob, setAddJob] = useState(false);
     const [showAddWorkshop, setAddWorkshop] = useState(false);
     const [course, setCourse] = useState("");
     const [instructor, setInstructor] = useState("");
     const [courseUrl, setCourseUrl] = useState("");
     const [job, setJob] = useState("");
-    const [jobEmployer, setEmployer] = useState("");
+    const [employer, setEmployer] = useState("");
     const [jobUrl, setJobUrl] = useState("");
     const [desc, setDesc] = useState("");
     const [revealed, setRevealed] = useState(false);
@@ -73,10 +73,74 @@ const DashboardLister = () => {
             let res = await instance.get("/api/jobs");
 
             console.log(res.data);
-            setJob(res.data);
+            setJobs(res.data);
         } catch (e) {
             console.log(e);
         }
+    };
+
+    const deleteCourse = async (id) => {
+        try {
+            let res = await instance.delete(`/api/courses/${id}`);
+
+            console.log(res.data);
+            fetchWorkshops();
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const deleteJob = async (id) => {
+        try {
+            let res = await instance.delete(`/api/jobs/${id}`);
+
+            console.log(res.data);
+            fetchJobs();
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const submitFormWorkshop = async () => {
+        setLoading(true);
+
+        try {
+            let res = await instance.post("/api/courses", {
+                name: course,
+                desc,
+                instructor,
+                url: courseUrl,
+                lister: AsyncStorage.getItem("username"),
+            });
+
+            console.log(res.data);
+        } catch (e) {
+            console.log(e);
+        }
+
+        setDesc("");
+        setLoading(false);
+    };
+
+    const submitFormJob = async () => {
+        setLoading(true);
+
+        try {
+            let res = await instance.post("/api/jobs", {
+                name: job,
+                desc,
+                employer,
+                url: jobUrl,
+                lister: AsyncStorage.getItem("username"),
+            });
+
+            console.log(res.data);
+        } catch (e) {
+            console.log(e);
+        }
+
+        setDesc("");
+        setLoading(false);
     };
 
     useEffect(() => {
@@ -125,13 +189,13 @@ const DashboardLister = () => {
                         backLayer={
                             <View
                                 style={{
-                                    height: 80,
+                                    height: 190,
                                     marginBottom: -10,
                                     padding: 12,
                                     backgroundColor: "#8e24aa",
                                 }}
                             >
-                                <Stack>
+                                <Stack spacing={12}>
                                     <HStack
                                         direction="row"
                                         alignItems="center"
@@ -148,8 +212,10 @@ const DashboardLister = () => {
                                         <Switch
                                             value={showWorkshop}
                                             onValueChange={() => {
-                                                setShowWorkshop(!showWorkshop);
-                                                setShowJob(!showJob);
+                                                setShowWorkshop(true);
+                                                setShowJob(false);
+                                                setAddWorkshop(false);
+                                                setAddJob(false);
                                                 fetchWorkshops();
                                             }}
                                         />
@@ -170,9 +236,57 @@ const DashboardLister = () => {
                                         <Switch
                                             value={showJob}
                                             onValueChange={() => {
-                                                setShowWorkshop(!showWorkshop);
-                                                setShowJob(!showJob);
+                                                setShowWorkshop(false);
+                                                setShowJob(true);
+                                                setAddWorkshop(false);
+                                                setAddJob(false);
                                                 fetchJobs();
+                                            }}
+                                        />
+                                    </HStack>
+                                    <HStack
+                                        direction="row"
+                                        alignItems="center"
+                                        justifyContent="space-between"
+                                    >
+                                        <Text
+                                            style={{
+                                                color: "#fff",
+                                                fontSize: "16px",
+                                            }}
+                                        >
+                                            Add Workshop
+                                        </Text>
+                                        <Switch
+                                            value={showAddWorkshop}
+                                            onValueChange={() => {
+                                                setShowWorkshop(false);
+                                                setShowJob(false);
+                                                setAddWorkshop(true);
+                                                setAddJob(false);
+                                            }}
+                                        />
+                                    </HStack>
+                                    <HStack
+                                        direction="row"
+                                        alignItems="center"
+                                        justifyContent="space-between"
+                                    >
+                                        <Text
+                                            style={{
+                                                color: "#fff",
+                                                fontSize: "16px",
+                                            }}
+                                        >
+                                            Add Job
+                                        </Text>
+                                        <Switch
+                                            value={showAddJob}
+                                            onValueChange={() => {
+                                                setShowWorkshop(false);
+                                                setShowJob(false);
+                                                setAddWorkshop(false);
+                                                setAddJob(true);
                                             }}
                                         />
                                     </HStack>
@@ -280,10 +394,10 @@ const DashboardLister = () => {
                                                                                 />
                                                                             )}
                                                                             onPress={() => {
-                                                                                deleteJob(
+                                                                                deleteCourse(
                                                                                     workshop.id
                                                                                 );
-                                                                                fetchJobs();
+                                                                                fetchWorkshops();
                                                                             }}
                                                                         />
                                                                     </Box>
@@ -384,10 +498,10 @@ const DashboardLister = () => {
                                                                             />
                                                                         )}
                                                                         onPress={() => {
-                                                                            deleteUsers(
+                                                                            deleteJob(
                                                                                 jobx.id
                                                                             );
-                                                                            fetchUsers();
+                                                                            fetchJobs();
                                                                         }}
                                                                     />
                                                                 </Box>
@@ -418,40 +532,97 @@ const DashboardLister = () => {
                                                 textContentType="none"
                                                 variant="standard"
                                                 color="#8e24aa"
-                                                onChangeText={setFname}
-                                                value={fname}
+                                                onChangeText={setCourse}
+                                                value={course}
                                             />
                                             <TextInput
                                                 label="Instructor Name"
                                                 textContentType="none"
                                                 variant="standard"
                                                 color="#8e24aa"
-                                                onChangeText={setLname}
-                                                value={lname}
+                                                onChangeText={setInstructor}
+                                                value={instructor}
                                             />
                                             <TextInput
                                                 label="Course URL"
                                                 textContentType="URL"
                                                 variant="standard"
                                                 color="#8e24aa"
-                                                onChangeText={setUsername}
-                                                value={username}
+                                                onChangeText={setCourseUrl}
+                                                value={courseUrl}
                                             />
                                             <TextInput
                                                 label="Description"
                                                 textContentType="none"
                                                 variant="standard"
                                                 color="#8e24aa"
-                                                secureTextEntry={true}
-                                                onChangeText={setPassword}
-                                                value={password}
+                                                onChangeText={setDesc}
+                                                value={desc}
                                             />
                                             <Button
                                                 title="Add Course"
                                                 variant="contained"
                                                 color="#fb8c00"
                                                 titleStyle={{ color: "#fff" }}
-                                                onPress={submitForm}
+                                                onPress={submitFormWorkshop}
+                                                disabled={loading}
+                                                loading={loading}
+                                            ></Button>
+                                        </Stack>
+                                    </Flex>
+                                </ScrollView>
+                            )}
+                        {!showWorkshop &&
+                            !showJob &&
+                            showAddJob &&
+                            !showAddWorkshop && (
+                                <ScrollView>
+                                    <Flex
+                                        fill
+                                        justifyContent="center"
+                                        style={{
+                                            padding: 18,
+                                        }}
+                                    >
+                                        <Stack spacing={18}>
+                                            <TextInput
+                                                label="Job Name"
+                                                textContentType="none"
+                                                variant="standard"
+                                                color="#8e24aa"
+                                                onChangeText={setJob}
+                                                value={job}
+                                            />
+                                            <TextInput
+                                                label="Employer Name"
+                                                textContentType="none"
+                                                variant="standard"
+                                                color="#8e24aa"
+                                                onChangeText={setEmployer}
+                                                value={employer}
+                                            />
+                                            <TextInput
+                                                label="Job URL"
+                                                textContentType="URL"
+                                                variant="standard"
+                                                color="#8e24aa"
+                                                onChangeText={setJobUrl}
+                                                value={jobUrl}
+                                            />
+                                            <TextInput
+                                                label="Description"
+                                                textContentType="none"
+                                                variant="standard"
+                                                color="#8e24aa"
+                                                onChangeText={setDesc}
+                                                value={desc}
+                                            />
+                                            <Button
+                                                title="Add Job"
+                                                variant="contained"
+                                                color="#fb8c00"
+                                                titleStyle={{ color: "#fff" }}
+                                                onPress={submitFormJob}
                                                 disabled={loading}
                                                 loading={loading}
                                             ></Button>
